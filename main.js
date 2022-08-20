ef83dff86b8388937dc3de45f971b7dbd607959d
Mikhail Yudin <fagci.nsk@gmail.com>
2022-08-21 02:27:13 +0700

    refactor: simplify

--- main.js
+++ main.js
@@ -31,24 +31,29 @@ class Connection extends net.Socket {
 function task() {
     let conn;
     setInterval(function() {
-        if (!conn) {
-            conn = new Connection(gen(), 80);
+        if (conn) return;
 
-            conn.addListener('connect', function() {
-                conn.send(`GET / HTTP/1.1\r\nHost: ${conn.remoteAddress}\r\n\r\n`);
-            });
+        conn = new Connection(gen(), 80);
 
-            conn.addListener('close', function() {
-                let data = conn.recv();
-                conn = null;
-                if (!data) return;
-                let m = data.match(/<title>([^<]+)/);
-                if (!m) return;
-                let title = m[1];
+        conn.addListener('connect', function() {
+            conn.send(`GET / HTTP/1.1\r\nHost: ${conn.remoteAddress}\r\n\r\n`);
+        });
 
-                console.log(title);
-            });
-        }
+        conn.addListener('close', function() {
+            let data = conn.recv();
+            let ip = conn.remoteAddress;
+
+            conn = null;
+
+            if (!data) return;
+
+            let m = data.match(/<title>([^<]+)/);
+            if (!m) return;
+
+            let title = m[1].replace(/(\r|\n)/, ' ').trim();
+
+            console.log(`${ip}: ${title}`);
+        });
     });
 };
 
