780a9c1c4c05bf8f68f130d112d654ca5d1ce857
Mikhail Yudin <fagci.nsk@gmail.com>
2022-08-21 13:11:56 +0700

    refactor: simplify

--- connection.js
+++ connection.js
@@ -3,24 +3,21 @@ const net = require('net');
 class Connection extends net.Socket {
     constructor(ip, port, connTimeout = 750, opTimeout = 5000) {
         super();
-        this.setTimeout(connTimeout);
         this.setNoDelay(true);
-        this.setKeepAlive(false);
+        this.setTimeout(connTimeout);
         this._recved = '';
         this._opTimeout = opTimeout;
 
         this.addListener('error', this.destroy);
         this.addListener('timeout', this.destroy);
+        this.addListener('close', this.clearOpTimeout);
         this.addListener('data', data => this._recved += data);
-        this.addListener('close', () => this.clearOpTimeout())
 
         this.connect(port, ip);
     }
 
     setOpTimeout() {
-        this._opTimeoutHandle = setTimeout(() => {
-            this.destroy();
-        }, this._opTimeout);
+        this._opTimeoutHandle = setTimeout(this.destroy, this._opTimeout);
     }
 
     clearOpTimeout() {
