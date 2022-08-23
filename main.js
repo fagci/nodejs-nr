e59ccf92c310591be04bb06c06db06a37fb3e6ba
Mikhail Yudin <fagci.nsk@gmail.com>
2022-08-23 23:52:41 +0700

    feat: rtsp

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const gen = require('./gen');
 const Connection = require('./connection');
 
+const FAKE_PATH = '/' + Math.random().toString(36).slice(2, 7);
+
 const PATHS = `
 /1
 /0/1:1/main
@@ -21,14 +23,12 @@ function sendRequest() {
     const path = this._paths.shift();
     this._path = path;
     this._cseq++;
-    if (this._cseq > 1) console.log(`${this.remoteAddress} req: ${this._cseq}`)
     this.send(
         `DESCRIBE rtsp://${this.remoteAddress}${path} RTSP/1.0\r\n`
         + `Accept: application/sdp\r\n`
         + `CSeq: ${this._cseq}\r\n`
         + `User-Agent: Lavf59.16.100\r\n\r\n`
     );
-    this.end();
 }
 
 function processResponse() {
@@ -41,12 +41,13 @@ function processResponse() {
     }
 
     const [_, proto, code, msg] = m;
-    if (code) console.log(`${this.remoteAddress}: ${this._path} ${code}`);
 
-    if (this._paths.length == 0 || code == 403) {
+    if (!proto.match(/^RTSP/) || this._paths.length == 0 || code == 403 || (this._path == FAKE_PATH && code[0] == 2)) {
         this.destroy();
         return;
     }
+
+    if (code[0] == 2) console.log(`rtsp://${this.remoteAddress}${this._path} ${code}`);
 
     sendRequest.bind(this)();
 }
@@ -55,10 +56,10 @@ function task() {
     const conn = new Connection(gen(), 554);
 
     conn._cseq = 0;
-    conn._paths = [...PATHS];
+    conn._paths = [FAKE_PATH, ...PATHS];
 
     conn.addListener('connect', sendRequest);
-    conn.addListener('end', processResponse);
+    conn.addListener('data', processResponse);
     conn.addListener('close', task);
 };
 
